f316281ff105d03fb4b92f13fd80629e2143b22c
Zhangeldy <Jangeldy@hotmail.com>
2018-09-06 23:37:27 +0600

    2nd stage of vueBlogging

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,18 @@
       <h1>Vue.js SPA</h1>
     </header>
     <main>
-      <aside class="sidebar"></aside>
-      <div class="content"></div>
+      <aside class="sidebar">
+        <router-link
+          v-for="post in posts"
+          active-class="is-active"
+          class="link"
+          :to="{name:'post',params:{id: post.id}}">
+          {{post.id}}.{{post.title}}
+        </router-link>
+      </aside>
+      <div class="content">
+        <router-view></router-view>
+      </div>
     </main>
   </div>
 </template>
